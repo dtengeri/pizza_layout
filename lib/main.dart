31b9bfe1268f5d89e91143ca1a18f6baf42c7775
dtengeri <dtengeri@gmail.com>
2021-05-13 10:05:31 +0200

    Integrate pizza bloc to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,36 @@
+import 'package:dio/dio.dart';
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pizza_layout/bloc/cartBloc.dart';
+import 'package:pizza_layout/bloc/pizza/pizza_api_client.dart';
+import 'package:pizza_layout/bloc/pizza/pizza_bloc.dart';
+import 'package:pizza_layout/bloc/pizza/pizza_event.dart';
+import 'package:pizza_layout/bloc/pizza/pizza_repository.dart';
 import 'package:pizza_layout/bloc/simple_bloc_observer.dart';
 import 'package:pizza_layout/router.dart';
 
 void main() {
   Bloc.observer = SimpleBlocObserver();
-  runApp(MyApp());
+  final pizzaRepository = PizzaRepository(
+    PizzaApiClient(
+      Dio(),
+    ),
+  );
+  runApp(MyApp(
+    pizzaRepository: pizzaRepository,
+  ));
 }
 
 class MyApp extends StatefulWidget {
+  final PizzaRepository pizzaRepository;
+
+  const MyApp({
+    Key? key,
+    required this.pizzaRepository,
+  }) : super(key: key);
   // This widget is the root of your application.
   @override
   _MyAppState createState() => _MyAppState();
@@ -29,8 +47,19 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => CartBloc(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          lazy: false,
+          create: (context) => PizzaBloc(widget.pizzaRepository)
+            ..add(
+              PizzaListRequested(),
+            ),
+        ),
+        BlocProvider(
+          create: (context) => CartBloc(),
+        )
+      ],
       child: MaterialApp(
         title: 'Pizza',
         theme: ThemeData(
